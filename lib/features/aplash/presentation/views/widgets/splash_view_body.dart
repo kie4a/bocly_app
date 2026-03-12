4ca8a5761047f47ace9100ea1f7233a30113e996
Mahdisalah <saaess33@gmail.com>
2026-03-12 15:08:31 +0300

    refactor init state & navigate to home

--- lib/features/aplash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/aplash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:app/constants.dart';
 import 'package:app/core/utils/assets.dart';
+import 'package:app/features/aplash/presentation/home/presentataion/views/view_models/home_view.dart';
 import 'package:app/features/aplash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -18,24 +21,17 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initState() {
     super.initState();
 
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(minutes: 1),
-    );
+    initSlidingAnimation();
 
-    slidingAnimation = Tween<Offset>(
-      begin: const Offset(0, 10),
-      end: Offset.zero,
-    ).animate(animationController);
-    
+    NavigateToHome();
   }
 
-
-@override
+  @override
   void dispose() {
-    
+    animationController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -47,5 +43,28 @@ class _SplashViewBodyState extends State<SplashViewBody>
         slidingtext(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  void NavigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fadeIn,
+        duration: kTranstionDuration,
+      );
+    });
   }
 }
