8e6794341048f61ac68031ebed0d898cee38d320
Mahdisalah <saaess33@gmail.com>
2026-03-23 19:10:41 +0300

    Create home tepo methods && failure file

--- lib/features/home/data/models/book_model/repos/home_repo.dart
+++ lib/features/home/data/models/book_model/repos/home_repo.dart
@@ -1,8 +1,15 @@
+import 'package:app/core/errors/failures.dart';
+import 'package:app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-
-  fetchFeaturedBooks();
+ Future<Either< Failures , List<BookModel > >>fetchBestSellerBooks(
 
 
-  
+ );
+ Future<Either<Failures,List<BookModel>>>  fetchFeaturedBooks();
+
+
+
+
 }
