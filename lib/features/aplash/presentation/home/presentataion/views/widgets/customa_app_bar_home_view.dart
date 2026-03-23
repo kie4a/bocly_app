7b274fdd871a68c1117c912ecc07f562ced02018
Mahdisalah <saaess33@gmail.com>
2026-03-23 15:21:49 +0300

    create custom search text field

--- lib/features/aplash/presentation/home/presentataion/views/widgets/customa_app_bar_home_view.dart
+++ lib/features/aplash/presentation/home/presentataion/views/widgets/customa_app_bar_home_view.dart
@@ -1,7 +1,8 @@
-
+import 'package:app/core/utils/app_router.dart';
 import 'package:app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBarHomeView extends StatelessWidget {
   const CustomAppBarHomeView({super.key});
@@ -9,16 +10,19 @@ class CustomAppBarHomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 40,bottom: 16),
-      child: Row( 
+      padding: const EdgeInsets.only(top: 40, bottom: 16),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Image.asset(AssetsDate.logo,height: 30,),
+          Image.asset(AssetsDate.logo, height: 30),
           IconButton(
-            
-              onPressed: () {},
-              icon: const FaIcon(FontAwesomeIcons.magnifyingGlass,size: 30,),
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
+            icon: const FaIcon(FontAwesomeIcons.magnifyingGlass, size: 26
+          ,color: Colors.white,
             ),
+          ),
         ],
       ),
     );
--- lib/features/aplash/presentation/search/presentation/views/widget/search_view_body.dart
+++ lib/features/aplash/presentation/search/presentation/views/widget/search_view_body.dart
@@ -1,10 +1,15 @@
+import 'package:app/features/aplash/presentation/search/presentation/views/widget/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(children: [const CustomSearchTextField()]),
+    );
   }
 }
