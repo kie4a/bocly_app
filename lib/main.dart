7d17a16158a18e6b1926e00e1d0656ac9cc05270
Mahdisalah <saaess33@gmail.com>
2026-03-26 21:48:11 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() { 
+  
   setupServicLocat();
   runApp(const BoolApp());
 }
