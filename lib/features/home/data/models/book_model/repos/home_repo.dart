32dec3109de2803d14b86ebddbf492a551e5bad0
Mahdisalah <saaess33@gmail.com>
2026-03-26 04:24:54 +0300

    implemnt fetch newset books

--- lib/features/home/data/models/book_model/repos/home_repo.dart
+++ lib/features/home/data/models/book_model/repos/home_repo.dart
@@ -3,13 +3,6 @@ import 'package:app/features/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
- Future<Either< Failures , List<BookModel > >>fetchBestSellerBooks(
-
-
- );
- Future<Either<Failures,List<BookModel>>>  fetchFeaturedBooks();
-
-
-
-
+  Future<Either<Failures, List<BookModel>>> fetchNewsetBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/features/home/data/models/book_model/repos/home_repo_impl.dart
+++ lib/features/home/data/models/book_model/repos/home_repo_impl.dart
@@ -1,13 +1,27 @@
 import 'package:app/core/errors/failures.dart';
+import 'package:app/core/utils/api_service.dart';
 import 'package:app/features/home/data/models/book_model/book_model.dart';
 import 'package:app/features/home/data/models/book_model/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint:
+            "volumes?q=flutter&key=AIzaSyAIwm6T4AEk_yAygBjp6aoi7a0hHhUAtsQ&Sorting=subject:Programming",
+      );
+      List<BookModel> books = [];
+      for (var item in data["item"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
@@ -15,5 +29,4 @@ class HomeRepoImpl implements HomeRepo {
     // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
-  
 }
